5620b0a870b295fa98451e42cf1823c5d17ef64f
christiankoback <chris_koback@hotmail.com>
2013-11-30 09:21:55 -1000

    For the main

---
+++ main(proj1).cpp
@@ -0,0 +1,38 @@
+//ENSE 274 lab project
+//DCS Development : Chris Koback ,Sean Boczulak ,Dirk Schmitz 
+//
+//Project Name: AirX, Airport Expert
+//
+
+#include<iostream>		// communicating with the user
+#include<fstream>		// if we read in a file
+
+#include<string>		// for the use of strings
+
+using namespace std;
+
+
+int main()
+{
+	// get all information about the chosen airport
+	// store it in classes ( map information stored in MAP CLASS... flight numbers, destination, airline, flight information in FLIGHT CLASS)
+
+	//ask the user for destination and airline
+
+	// search for the destination and the airline in the FLIGHT CLASS and return
+	//all accessible flight information 
+
+	// if no entered destination OR airline, ask the user for flight number 
+
+	// search for the flight number in the FLIGHT CLASS and return
+	//all accessible flight information
+
+	// get user's location (GPS or ask for the closest place to the user)
+	// show the map for the specific airport from MAP CLASS
+
+	// ask the user for place of interests
+
+	// if so, show the map with the additional places of interests from MAP CLASS
+
+	return 0;
+}
