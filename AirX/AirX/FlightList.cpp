df127cdb6d7ed88efb5beeefa096bddb53b3b118
sbocz <sean_bocz@hotmail.com>
2013-11-30 18:38:08 -0600

    FlightList
    Made compatable with new flight changes

--- AirX/AirX/FlightList.cpp
+++ AirX/AirX/FlightList.cpp
@@ -8,36 +8,42 @@ FlightList::FlightList(string FL)
 {
 	ifstream FlightFile;
 	FlightFile.open("Flight.txt");
-	long unsigned num;
+	string num;
 	string dest;
 	string air;
 	string info;
+	string connections;
+	string time;
+	string start;
 
 	for (int i = 0; i < FLIGHTS; i++)
 	{
 		FlightFile >> num;
 		FlightFile >> dest;
 		FlightFile >> air;
-		FlightFile >> info;
-		List[i] = Flight(num, dest, air, info);
+		FlightFile >> connections;
+		FlightFile >> time;
+		FlightFile >> start;
+		Flight temp(num, dest, air, connections, time, start);
+		List[i] = temp;
 	}
 }
 
-void PrintList()const
+void FlightList::PrintList()const
 {
 	for (int i = 0; i < FLIGHTS; i++)
 	{
-		List[i].PrintFlightInfo();
+		List[i].PrintFlightShort();
 	}
 }
 
-void PrintFiltered(string destinationIn, string airlineIn)const
+void FlightList::PrintFiltered(string destinationIn, string airlineIn)const
 {
 	for (int i = 0; i < FLIGHTS; i++)
 	{
 		if (List[i].MeetsCriteria(destinationIn, airlineIn))
 		{
-			List[i].PrintFlightInfo();
+			List[i].PrintFlightShort();
 		}
 	}
 }
