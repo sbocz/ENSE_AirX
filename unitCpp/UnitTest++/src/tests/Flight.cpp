a2308eda648cb5093996244daa8ecd66bad0f127
christiankoback <chris_koback@hotmail.com>
2013-12-03 07:10:20 -1000

    updated files

--- unitCpp/UnitTest++/src/tests/Flight.cpp
+++ unitCpp/UnitTest++/src/tests/Flight.cpp
@@ -2,51 +2,66 @@
 
 Flight::Flight()
 {
-	flightNumber = "0000";
-	destination = "";
-	airline = "";
-	connections = "";
-	time = "0:00";
-	start = "";
+        flightNumber = "0000";
+        destination = "";
+        airline = "";
+        connections = "";
+        time = "0:00";
+        start = "";
 }
 Flight::Flight(string flightNumber_in, string destination_in, string airline_in, string connections_in, string Time, string start_in)
 {
-	flightNumber = flightNumber_in;
-	destination = destination_in;
-	airline = airline_in;
-	connections = connections_in;
-	time = Time;
-	start = start_in;
+        flightNumber = flightNumber_in;
+        destination = destination_in;
+        airline = airline_in;
+        connections = connections_in;
+        time = Time;
+        start = start_in;
 }
+
+Flight& Flight::operator= (const Flight& original)
+{
+        flightNumber = original.flightNumber;
+        destination = original.destination;
+        airline = original.airline;
+        connections = original.connections;
+        time = original.time;
+        start = original.start;
+        return *this;
+}
+
 bool Flight::MeetsCriteria(string destinationIn, string airlineIn)const
 {
-	if(destination == destinationIn)
-	{
-		if(airlineIn == airline)
-		{
-			return true;
-		}
-		else
-		{
-		return false;
-		}
-	}
-	else
-	{
-		return false;
-	}
+        if(destination == destinationIn && airline == airlineIn)
+                return true;
+        if("x" == destinationIn && airline == airlineIn)
+                return true;
+        if(destination == destinationIn && "x" == airlineIn)
+                return true;
+        if("x" == destinationIn && "x" == airlineIn)
+                return true;
+        return false;
 }
 
 void Flight::PrintFlightLong()const
 {
-	cout<< "The connecting flights are " << connections << endl; 
-	cout<<"You leave at :  "<<time <<endl;
-	cout<<"You are flying with the airline called:  "<<airline<<endl;
-	cout<< "You are going to:  "<< destination<<endl;
-	cout<<"You leave from:  "<<start<<endl;
-	cout<<"Your flight number is: "<<flightNumber<<endl;
+        cout << "You are flying with " << airline << endl;
+        cout << "Flight Number: " << flightNumber << endl;
+        cout << "Your Destination is: "<< destination << endl;
+        cout << "Leaving From: " << start << endl;
+        cout << "Flight Departure at: "<< time << endl;
+        cout << "The connecting flights are in " << connections << endl;
 }
 void Flight::PrintFlightShort()const
 {
-	cout<<"Connecting flights: "<<connections<<"departure= "<<time<<"Airline is: "<<airline<<"Going to :"<<destination<<"Leaving from= "<<start<<"Flight number= "<<flightNumber<<endl<<endl;
+        cout << setw(6) << flightNumber << setw(12) << airline
+                 << setw(15) << destination << setw(10) << start
+                 << setw(13) << time << setw(14) << connections << endl;
+}
+
+bool Flight::Selected(string flightNum)const
+{
+        if (flightNumber == flightNum)
+                return true;
+        return false;
 }
--- unitCpp/UnitTest++/src/tests/Flight.h
+++ unitCpp/UnitTest++/src/tests/Flight.h
@@ -4,31 +4,38 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <iomanip>
 
 using namespace std;
 
 class Flight
 {
-	string connections;
-	string time;
-	string airline;
-	string destination;
-	string start;
-	string flightNumber;
+        string connections;
+        string time;
+        string airline;
+        string destination;
+        string start;
+        string flightNumber;
 
 public:
 
-	Flight();
+        Flight();
 
-	Flight(string flightNumber_in, string destination_in, string airline_in,string connections_in, string Time, string start_in );
+        Flight(string flightNumber_in, string destination_in, string airline_in,string connections_in, string Time, string start_in );
 
-	// search for the destination and the airline in the FLIGHT CLASS and return
-	// true if flight meets criteria 
-	bool MeetsCriteria(string destinationIn, string airlineIn)const;
+        //Assignment operator
+        Flight& operator= (const Flight& original);
+        
+        // search for the destination and the airline in the FLIGHT CLASS and return
+        // true if flight meets criteria
+        bool MeetsCriteria(string destinationIn, string airlineIn)const;
 
-	// print flight
-	void PrintFlightLong()const;
-	void PrintFlightShort()const;
+        // print flight
+        void PrintFlightLong()const;        //User Flight
+        void PrintFlightShort()const;        //List form
+
+        //Checks if a flight number is the same as the requested value
+        bool Selected(string)const;
 };
 
 #endif
